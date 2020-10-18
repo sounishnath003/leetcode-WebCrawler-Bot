8b8a92d42d5defaf028bc5e060bb6ee302e36b73
Sounish Nath <flock.sinasini@gmail.com>
2020-10-18 15:32:48 +0530

    removing bloatware imprts

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const $ = require("jquery")(window);
+const cheerio = require('cheerio');
 const axios = require("axios").default;
 const fs = require("fs");
 
 function fetchfromUrl(url) {
   try {
     const rawData = axios.get(url).then((res) => res.data);
-    const doc = cheerio.load(rawData);
-    return doc;
+    return rawData;
   } catch (error) {
     console.log(`Error Occured: ` + error);
   }
 }
 
 const customScrapper = async (docHTML) => {
-  const $ = cheerio.load(docHTML);
   console.log(`## Hold on... started Crawling...`);
+  const $ = cheerio.load(docHTML);
 
-  let title = $(".css-v3d350").__reactEventHandlers$ou4q4w0vnij.children[2];
-  console.log({ title });
+  // let title = $(".css-v3d350").__reactEventHandlers$ou4q4w0vnij.children[2];
+  // console.log(title);
 };
 
 const runner = async (url) => {
   const doc = await fetchfromUrl(url);
-  customScrapper(doc);
+  await customScrapper(doc);
 };
 
 function entryPoint() {
